08cb36f7c2892fb1c839760e910e72cde62879f8
Kusuu <94615250+kuscuumehmetEmir@users.noreply.github.com>
2024-08-12 19:49:35 -0700

    i updated the acces.log file

--- start/CH07/Find404.py
+++ start/CH07/Find404.py
@@ -1,3 +1,13 @@
-#!/usr/bin/env python3
-# Script that scans web server logs for 404 errors
-# By 
+import datetime
+
+def create_log_entry(ip_address, identd, username, resource_message, http_return_code, return_size):
+    timestamp = datetime.datetime.now().strftime("[%d/%b/%Y:%H:%M:%S %z]")
+
+    log_entry = f'{ip_address} {identd} {username} {timestamp} "{resource_message}" {http_return_code} {return_size}\n'
+
+    with open('access.log', 'a') as log_file:
+        log_file.write(log_entry)
+
+    print("Log entry added successfully.")
+
+create_log_entry('192.168.1.1', '-', 'johnnah', 'GET /index.html HTTP/1.1', '200', '1234')
